f4be4dfc69f181f9ab71001fa654c75673ef1c30
Abhasjaiswal <abhas.jaiswal0907@gmail.com>
2025-03-31 16:04:22 +0530

    update cors

--- src/app.js
+++ src/app.js
@@ -6,11 +6,12 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static("public"));
